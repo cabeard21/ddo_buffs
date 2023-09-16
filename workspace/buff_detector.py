132b595fb434435b3a028b8d554471c2dc149c27
Chad Beard <46787164+cabeard21@users.noreply.github.com>
2023-09-15 21:21:53 -0500

    add: logging functionality to buff_detector.py

--- workspace/buff_detector.py
+++ workspace/buff_detector.py
@@ -2,12 +2,18 @@ import cv2
 import numpy as np
 import os
 import pyautogui
+import logging
 
 class BuffDetector:
     def __init__(self, coordinates, buff_dir):
         self.coordinates = coordinates
         self.buff_dir = buff_dir
         self.templates = self.load_templates()
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        handler = logging.FileHandler('buff_detector.log')
+        handler.setLevel(logging.INFO)
+        self.logger.addHandler(handler)
 
     def load_templates(self):
         templates = {}
@@ -27,12 +33,15 @@ class BuffDetector:
             result = cv2.matchTemplate(screen[self.coordinates[1]:self.coordinates[3], self.coordinates[0]:self.coordinates[2]], template, cv2.TM_CCOEFF_NORMED)
             _, _, _, max_loc = cv2.minMaxLoc(result)
             buffs.append((name, max_loc))
+            self.logger.info(f"Match template result for {name}: {max_loc}")
 
         return buffs
 
     def capture_screen(self):
         # Capture the screen using pyautogui
         screenshot = pyautogui.screenshot()
+        # Save the screenshot for troubleshooting
+        screenshot.save('screenshot.png')
         # Convert the screenshot to grayscale
         screen = cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2GRAY)
         return screen
--- workspace/workspace/LAST_MODIFICATION_README.md
+++ workspace/workspace/LAST_MODIFICATION_README.md
@@ -1,9 +1,5 @@
-The error message indicates that the depth of the image should be either CV_8U or CV_32F and the type of the image should be the same as the template. Also, the image should be 2-dimensional. The error is occurring in the `detect` method of the `BuffDetector` class in the `buff_detector.py` file.
+To implement the requested functionality, we need to add logging to the BuffDetector class and also add a method to save the screenshot taken by the buff detector. 
 
-The `cv2.matchTemplate` function is used to match the template image with the source image. The source image is obtained from the `capture_screen` method which uses `pyautogui.screenshot` to capture the screen and then converts it to a BGR image using `cv2.cvtColor`. The template image is loaded using `cv2.imread` with a 0 as the second argument which loads the image in grayscale.
-
-The error can be fixed by ensuring that both the source image and the template image are in the same format. Since the template image is loaded in grayscale, the source image should also be converted to grayscale.
-
-Here is the modified `buff_detector.py` file:
+Here are the changes:
 
 buff_detector.py
