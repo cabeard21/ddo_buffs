1114a8efb127047ace97d386060e5d6df4ddbc88
Chad Beard <46787164+cabeard21@users.noreply.github.com>
2023-09-18 00:33:55 -0500

    edit: main.py works as a Qt widget now

--- workspace/main.py
+++ workspace/main.py
@@ -1,48 +1,82 @@
 import json
-from buff_detector import BuffDetector
-from buff_timer import BuffTimer
-from buff_bar import BuffBar
-from buff_sorter import BuffSorter
-from buff_ocr import BuffOCR
+import sys
 from pathlib import Path
+
+from buff_bar import BuffBar
+from buff_detector import BuffDetector
+from buff_ocr import BuffOCR
+from buff_sorter import BuffSorter
+from buff_timer import BuffTimer
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtWidgets import QApplication, QVBoxLayout, QWidget
 
 BASE = Path(__file__).resolve().parent
 
-def main():
-    # Load area of interest coordinates
-    with open(BASE / 'coordinates.json', 'r') as f:
-        coordinates = json.load(f)
+class MainApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
 
-    # Initialize classes
-    buff_dir = BASE / 'buffs'
-    template_dir = BASE / 'templates'
-    buff_detector = BuffDetector(coordinates, buff_dir)
-    buff_timer = BuffTimer()
-    buff_bar = BuffBar()
-    buff_sorter = BuffSorter()
-    buff_ocr = BuffOCR(template_dir)
+    def initUI(self):
+        self.layout = QVBoxLayout()
+        self.layout.setSpacing(5)  # Adjust this value as needed
+        self.setLayout(self.layout)
 
-    # Main loop
-    while True:
+        # Remove title bar, set background transparent, and make window always on top
+        self.setWindowFlags(Qt.FramelessWindowHint | Qt.Tool | Qt.WindowStaysOnTopHint)
+        self.setAttribute(Qt.WA_TranslucentBackground)
+
+        self.show()
+
+    def update_bars(self):
         # Detect buffs
-        buffs = buff_detector.detect()
+        buffs = self.buff_detector.detect()
 
         # Update timers and bars
         for buff, image_data in buffs:
             # Read timer
-            time = buff_ocr.read(image_data)
+            time = self.buff_ocr.read(image_data)
 
             # Update timer
-            buff_timer.update(buff, time)
+            self.buff_timer.update(buff, time)
 
             # Update bar
-            buff_bar.update(buff, buff_timer.get_remaining(buff))
+            self.buff_bar.update(buff, self.buff_timer.get_remaining(buff))
 
         # Sort bars
-        buff_sorter.sort(buff_bar.bars)
+        sorted_bars = self.buff_sorter.sort(self.buff_bar.bars)
+
+        # Update bars with sorted order
+        self.buff_bar.bars = dict(sorted_bars)
 
         # Display bars
-        buff_bar.display()
+        self.buff_bar.display()
+
+    def main(self):
+        # Load area of interest coordinates
+        with open(BASE / 'coordinates.json', 'r') as f:
+            coordinates = json.load(f)
+
+        # Initialize classes
+        buff_dir = BASE / 'buffs'
+        template_dir = BASE / 'templates'
+        self.buff_detector = BuffDetector(coordinates, buff_dir, 0.5)
+        self.buff_timer = BuffTimer()
+        self.buff_sorter = BuffSorter()
+        self.buff_ocr = BuffOCR(template_dir)
+
+        # Initialize BuffBar with tkinter root
+        self.buff_bar = BuffBar()
+
+        # QTimer to update the bars
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.update_bars)
+        self.timer.start(1000)  # Update every second
 
 if __name__ == "__main__":
-    main()
+    app = QApplication(sys.argv)
+    ex = MainApp()
+    ex.main()
+    sys.exit(app.exec_())
+
+
