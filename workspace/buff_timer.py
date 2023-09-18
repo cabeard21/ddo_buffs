ccd9bfcb6687df3d9eb83eefff6687f82ebe66ba
Chad Beard <46787164+cabeard21@users.noreply.github.com>
2023-09-18 00:34:35 -0500

    edit: fixed logic in buff_timer

--- workspace/buff_timer.py
+++ workspace/buff_timer.py
@@ -4,8 +4,11 @@ class BuffTimer:
     def __init__(self):
         self.timers = {}
 
-    def update(self, buff, time):
-        self.timers[buff] = time
+    def update(self, buff, time_value):
+        self.timers[buff] = time_value + time.time()
 
     def get_remaining(self, buff):
-        return max(0, self.timers[buff] - time.time())
+        if (time.time() - self.timers[buff]) > 0:
+            return 0
+        else:
+            return self.timers[buff] - time.time()
