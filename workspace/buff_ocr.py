f4fc282fa62452231f111811b1e60e0c637d11b0
Chad Beard <46787164+cabeard21@users.noreply.github.com>
2023-09-18 09:26:25 -0500

    edit: added NMS and duplication detection to buff_ocr

--- workspace/buff_ocr.py
+++ workspace/buff_ocr.py
@@ -5,6 +5,30 @@ import cv2
 import numpy as np
 
 
+def non_maximum_suppression(boxes, scores, threshold=0.5):
+    boxes = np.array([(x1, y1, x2 - x1, y2 - y1)
+                      for (x1, y1, x2, y2) in boxes])
+    areas = boxes[:, 2] * boxes[:, 3]
+    order = scores.argsort()[::-1]
+    keep = []
+    while order.size > 0:
+        i = order[0]
+        keep.append(i)
+        xx1 = np.maximum(boxes[i, 0], boxes[order[1:], 0])
+        yy1 = np.maximum(boxes[i, 1], boxes[order[1:], 1])
+        xx2 = np.minimum(boxes[i, 0] + boxes[i, 2],
+                         boxes[order[1:], 0] + boxes[order[1:], 2])
+        yy2 = np.minimum(boxes[i, 1] + boxes[i, 3],
+                         boxes[order[1:], 1] + boxes[order[1:], 3])
+        w = np.maximum(0.0, xx2 - xx1)
+        h = np.maximum(0.0, yy2 - yy1)
+        intersection = w * h
+        union = areas[i] + areas[order[1:]] - intersection
+        iou = intersection / union
+        order = order[np.where(iou <= threshold)[0] + 1]
+    return keep
+
+
 class BuffOCR:
 
     def __init__(self, template_dir, threshold=0.8):
@@ -30,40 +54,41 @@ class BuffOCR:
         return templates
 
     def read(self, buff, image_data):
-        # Save the image for debugging
         cv2.imwrite(
             os.path.join(os.path.dirname(os.path.realpath(__file__)),
                          'buff_image.png'), image_data)
-
-        # Detect all matched positions for each template
-        all_detected_positions = {}
+        all_boxes = []
+        all_scores = []
+        all_labels = []
         for key, template in self.templates.items():
             result = cv2.matchTemplate(image_data, template,
                                        cv2.TM_CCOEFF_NORMED)
             loc = np.where(result >= self.threshold)
-            all_detected_positions[key] = list(zip(loc[1], loc[0]))
-
-        # Flatten the dictionary and sort all positions based on x-coordinate
-        all_positions_list = [
-            (key, pos) for key, positions in all_detected_positions.items()
-            for pos in positions
-        ]
-        sorted_all_positions = sorted(all_positions_list,
-                                      key=lambda x: x[1][0])
-
-        # Extract detected time
-        detected_time = ''.join([item[0] for item in sorted_all_positions])
-
-        # Convert the detected time to seconds
+            for (x, y) in zip(loc[1], loc[0]):
+                all_boxes.append(
+                    (x, y, x + template.shape[1], y + template.shape[0]))
+                all_scores.append(result[y, x])
+                all_labels.append(key)
+        keep_indices = non_maximum_suppression(all_boxes,
+                                               np.array(all_scores),
+                                               threshold=0.5)
+        nms_boxes = [all_boxes[i] for i in keep_indices]
+        nms_labels = [all_labels[i] for i in keep_indices]
+        sorted_indices = sorted(range(len(nms_boxes)),
+                                key=lambda k: nms_boxes[k][0])
+        sorted_labels = [nms_labels[i] for i in sorted_indices]
+        detected_time = ''.join(sorted_labels)
+        if detected_time.count('colon') > 1:
+            parts = detected_time.split('colon')
+            detected_time = 'colon'.join([parts[0], parts[-1]])
         try:
             minutes, seconds = map(int, detected_time.split('colon'))
             total_seconds = minutes * 60 + seconds
         except ValueError:
-            self.logger.error(f'Error converting detected time to seconds '
-                              f'for buff {buff}: {detected_time}')
+            self.logger.error(
+                f'Error converting detected time to seconds for buff {buff}: '
+                f'{detected_time}')
             total_seconds = 0
-
         self.logger.debug(f'Detected time for buff {buff}: {detected_time} '
                           f'({total_seconds} seconds)')
-
         return total_seconds
