18cc892e93e8cc5f715a9ac5c84d71dc5af72793
Chad Beard <46787164+cabeard21@users.noreply.github.com>
2023-09-28 08:29:46 -0500

    edit: BuffTimer to check for timers that are misread initially.

--- workspace/buff_timer.py
+++ workspace/buff_timer.py
@@ -7,10 +7,13 @@ class BuffTimer:
         self.timers = {}
 
     def update(self, buff, time_value):
-        self.timers[buff] = time_value + time.time()
+        # Check if the new remaining time is greater than the current
+        # remaining time
+        if time_value > self.get_remaining(buff):
+            self.timers[buff] = time_value + time.time()
 
     def get_remaining(self, buff):
-        if (time.time() - self.timers[buff]) > 0:
+        if buff not in self.timers or (time.time() - self.timers[buff]) > 0:
             return 0
         else:
             return self.timers[buff] - time.time()
